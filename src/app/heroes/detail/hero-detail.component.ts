c5f77fd0884d72c61be4fa0f74bcef432c9afe33
semika <semika.siriwardana@shipxpress.com>
2017-02-17 19:02:57 +0530

    Validation Changed

--- src/app/heroes/detail/hero-detail.component.ts
+++ src/app/heroes/detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
 
-import { Component, Input , OnInit , Inject } from '@angular/core'; 
+import { Component, Input , OnInit , Inject , ViewChild , AfterViewChecked} from '@angular/core'; 
 import { ActivatedRoute, Params, Router} from '@angular/router';
 import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
+
 import 'rxjs/add/operator/switchMap';
 
 import { Hero } from '../model/hero'; 
@@ -14,7 +16,7 @@ declare var __moduleName: string;
     templateUrl: 'hero-detail.component.html',
     styleUrls : ['hero-detail.component.css'] 
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit , AfterViewChecked {
 
     constructor(private heroService: HeroService,
                 private route: ActivatedRoute,
@@ -23,6 +25,24 @@ export class HeroDetailComponent implements OnInit {
                 @Inject('powerListToken') public powers : {}) { } 
     @Input() 
     hero: Hero;
+
+    heroForm : NgForm;
+
+    formErrors = {
+        'name': '',
+        'power': ''
+    };
+
+    validationMessages = {
+        'name': {
+            'required':      'Name is required.'
+        },
+        'power': {
+            'required': 'Power is required.'
+        }
+    };
+
+    @ViewChild('heroForm') currentForm : NgForm;
     
     ngOnInit() : void {
         this.route.params.switchMap((params: Params) => this.heroService.getHeroById(+params['id'])).subscribe(hero => {
@@ -32,6 +52,37 @@ export class HeroDetailComponent implements OnInit {
                 this.hero = JSON.parse(JSON.stringify(hero)) // clone the object
             }
         }); 
+    }
+
+    ngAfterViewChecked() : void {
+        this.formChanged();
+    }
+
+    formChanged() : void {
+        if (this.currentForm === this.heroForm) { return; }
+        this.heroForm = this.currentForm;
+        if (this.heroForm) {
+            this.heroForm.valueChanges.subscribe(data => this.onValueChanged(data));
+        }
+    }
+
+    onValueChanged(data?: any) : void {
+        if (!this.heroForm) { return; }
+        
+        const form = this.heroForm.form;
+
+        for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
     }
 
     goBack() : void {
--- src/app/students/detail/student-detail.component.ts
+++ src/app/students/detail/student-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component , OnInit, Inject } from '@angular/core';
+import { Component , OnInit, Inject , ViewChild , AfterViewChecked} from '@angular/core';
 import { ActivatedRoute, Params, Router} from '@angular/router';
 import { Location } from '@angular/common'; 
+import { NgForm } from '@angular/forms';
 
 import { Student} from '../model/student';
 import { StudentService} from '../service/student.service';
@@ -12,15 +13,36 @@ declare var __moduleName: string;
     templateUrl : 'student-detail.component.html',
     styleUrls: ['student-detail.component.css']
 })
-export class StudentDetailComponent implements OnInit {
+export class StudentDetailComponent implements OnInit , AfterViewChecked {
 
     student: Student;
+
+    studentForm : NgForm;
+
+    formErrors = {
+        'name': '',
+        'gender': ''
+    };
+
+    validationMessages = {
+        'name': {
+            'required':      'Name is required.',
+            'minlength':     'Name must be at least 4 characters long.',
+            'maxlength':     'Name cannot be more than 24 characters long.',
+            'forbiddenName': 'Someone named "Bob" cannot be a hero.'
+        },
+        'gender': {
+            'required': 'Power is required.'
+        }
+    };
 
     constructor(private studentService : StudentService, 
                 private route : ActivatedRoute, 
                 private location: Location, 
                 @Inject('genderList') public genderList : {},
                 private router : Router) { }
+
+    @ViewChild('studentForm') currentForm : NgForm;
 
     ngOnInit() : void {
         this.route.params.switchMap((params: Params) => this.studentService.getStudentById(+params['id'])).subscribe(student => {
@@ -30,6 +52,37 @@ export class StudentDetailComponent implements OnInit {
                 this.student = JSON.parse(JSON.stringify(student)); 
             }
         }); 
+    }
+
+    ngAfterViewChecked() : void {
+        this.formChanged();
+    }
+
+    formChanged() : void {
+        if (this.currentForm === this.studentForm ) { return; }
+        this.studentForm  = this.currentForm;
+        if (this.studentForm ) {
+            this.studentForm .valueChanges.subscribe(data => this.onValueChanged(data));
+        }
+    }
+
+     onValueChanged(data?: any) : void {
+        if (!this.studentForm) { return; }
+        
+        const form = this.studentForm.form;
+
+        for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
     }
 
     add() { 
